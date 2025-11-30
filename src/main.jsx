c5718704f6984851ed687187cf3a27faacbffe25
Benjamin Burt <bburt85@gmail.com>
2025-11-30 00:40:53 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import WonderOSComplete from './WonderOSComplete.jsx'
+import './index.css'  // Optional: Add Tailwind or basic CSS if needed
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WonderOSComplete />
+  </React.StrictMode>,
+)
